c3c29b0a0e725f286e6bc52a788ab9a8e8cada8b
kff11 <55500009+kff11@users.noreply.github.com>
2020-06-03 14:46:47 +0900

    제 n사분면 구하기

--- src/If/Quadrant.java
+++ src/If/Quadrant.java
@@ -1,4 +1,24 @@
 package If;
 
+import java.util.Scanner;
+
 public class Quadrant {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        if (x > 0 && y > 0) {
+            System.out.print(1);
+        } else if (x < 0 && y > 0) {
+            System.out.print(2);
+        } else if (x < 0 && y < 0) {
+            System.out.print(3);
+        } else if (x > 0 && y < 0) {
+            System.out.print(4);
+        }
+
+    }
 }
